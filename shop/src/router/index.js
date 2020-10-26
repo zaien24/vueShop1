9c804a9d28ec31bc4b94577452148abe735eb265
zaien24 <zaien24@gmail.com>
2020-10-26 23:14:38 +0900

    초기세팅 완료

--- shop/src/router/index.js
+++ shop/src/router/index.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import Home from '../views/Home.vue'
 
@@ -6,14 +7,6 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
   }
 ]
 
--- shop/src/store/index.js
+++ shop/src/store/index.js
@@ -1,3 +1,4 @@
+import Vue from 'vue'
 import { createStore } from 'vuex'
 
 export default createStore({
