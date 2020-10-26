7f1760fbdc8f2edd520a244a16d1788be6b2a72a
zaien24 <zaien24@gmail.com>
2020-10-26 23:38:39 +0900

    레이아웃 설정 완료

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,3 +1,16 @@
 <template>
-  
+    <div id="app">
+        <Header />
+        <Footer />
+    </div>
 </template>
+<script>
+import Header from '@/components/Header.vue';
+import Footer from '@/components/Footer.vue';
+export default {
+    components: {
+      Header,
+      Footer
+    }
+}
+</script>
