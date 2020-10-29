ee5f647c50150058b89248724bb4f8c59f01b8ab
zaien24 <zaien24@gmail.com>
2020-10-29 23:21:59 +0900

    1차_2. 템플릿 구조_201029

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -8,6 +8,14 @@
 import Header from '@/components/Header.vue';
 import Footer from '@/components/Footer.vue';
 export default {
+    mounted() {
+        $(".selection-1").select2({
+            minimumResultsForSearch: 20,
+            dropdownParent: $('#dropDownSelect1')
+        });
+
+        $.initialize();
+    },
     components: {
       Header,
       Footer
