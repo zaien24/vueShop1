7e27bc332a5fb58ba39608b69d3b684c6dfd049d
zaien24 <zaien24@gmail.com>
2020-10-29 23:42:10 +0900

    1차_3. 라우터 적용하기_201029_2

--- shop/src/components/Header.vue
+++ shop/src/components/Header.vue
@@ -41,37 +41,16 @@
 				<div class="wrap_menu">
 					<nav class="menu">
 						<ul class="main_menu">
-							<li>
-								<a href="index.html">Home</a>
-								<ul class="sub_menu">
-									<li><a href="index.html">Homepage V1</a></li>
-									<li><a href="home-02.html">Homepage V2</a></li>
-									<li><a href="home-03.html">Homepage V3</a></li>
-								</ul>
-							</li>
+							<router-link :to="{name: 'Home'}" tag="li" active-class="sale-noti" exact>
+								<a>Home</a>
+							</router-link>
 
-							<li>
-								<a href="product.html">Shop</a>
-							</li>
-
-							<li class="sale-noti">
-								<a href="product.html">Sale</a>
-							</li>
+							<router-link :to="{name: 'shop'}" tag="li" active-class="sale-noti" exact>
+								<a>Shop</a>
+							</router-link>
 
 							<li>
 								<a href="cart.html">Features</a>
-							</li>
-
-							<li>
-								<a href="blog.html">Blog</a>
-							</li>
-
-							<li>
-								<a href="about.html">About</a>
-							</li>
-
-							<li>
-								<a href="contact.html">Contact</a>
 							</li>
 						</ul>
 					</nav>
