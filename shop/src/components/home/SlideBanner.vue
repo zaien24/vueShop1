d074161be4d1106a90b5f5b969b253231b973f19
zaien24 <zaien24@gmail.com>
2020-10-31 09:52:39 +0900

    4.API 호출해서 배너 출력하기_3

--- shop/src/components/home/SlideBanner.vue
+++ shop/src/components/home/SlideBanner.vue
@@ -1,16 +1,16 @@
 <template>
     <section class="slide1">
 		<div class="wrap-slick1">
-			<div class="slick1">
+			<div class="slick1" ref="slick">
                 <template v-for="banner in banners">
-                    <div class="item-slick1 item1-slick1" style="background-image: url(images/master-slide-02.jpg);">
+                    <div class="item-slick1 item1-slick1" :style="`background-image: url(${banner.image});`">
                         <div class="wrap-content-slide1 sizefull flex-col-c-m p-l-15 p-r-15 p-t-150 p-b-170">
                             <span class="caption1-slide1 m-text1 t-center animated visible-false m-b-15" data-appear="fadeInDown">
-                                Women Collection 2018
+                                {{ banner.subtitle }}
                             </span>
 
                             <h2 class="caption2-slide1 xl-text1 t-center animated visible-false m-b-37" data-appear="fadeInUp">
-                                New arrivals
+                                {{ banner.title }}
                             </h2>
 
                             <div class="wrap-btn-slide1 w-size1 animated visible-false" data-appear="zoomIn">
@@ -42,6 +42,11 @@ export default {
             //this.banners = response.data;
 
             //response.data.push(123);
+
+            this.$nextTick(() => {
+                $(this.$refs.slick).slick1();
+            }) 
+            
         }) 
     }
 }
